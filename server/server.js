52a2a6946c5dc551085f67f177e62c94edb2a6de
Dnyaneshwar150 <dnyaneshwardimble25436@gmail.com>
2024-03-09 16:41:19 +0530

    deploy fix

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ db.on('disconnected', () => {
 // Middleware
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
