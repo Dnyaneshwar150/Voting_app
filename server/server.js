478b97a68827caf6fe9a94a925ac9f18335bf141
Dnyaneshwar150 <dnyaneshwardimble25436@gmail.com>
2024-03-09 16:35:54 +0530

    error fix cors

--- server/server.js
+++ server/server.js
@@ -36,7 +36,14 @@ db.on('disconnected', () => {
 
 // Middleware
 app.use(cors());
-  app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+app.use(express.json());
 app.use(bodyParser.json());
 
 // Routes
